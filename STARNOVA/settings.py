a455be665d70e00e236680523de84aab23ab4dfb
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-01-07 20:28:57 +0600

    Util Deleted

--- STARNOVA/settings.py
+++ STARNOVA/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'utils',
     'api',
     'rest_framework.authtoken',
     'rest_framework',
