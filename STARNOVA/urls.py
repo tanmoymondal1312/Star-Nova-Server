7b35e99317c4a2b45060c3dbb9b1b9814108b6af
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-01-07 23:46:51 +0600

    View Updated

--- STARNOVA/urls.py
+++ STARNOVA/urls.py
@@ -1,7 +1,9 @@
+from django import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
