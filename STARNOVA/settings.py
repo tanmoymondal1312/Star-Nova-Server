ff6399a586dcdf6816544e967e588ae50da8c9ac
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-01-07 20:31:28 +0600

    API UTIL Deleted

--- STARNOVA/settings.py
+++ STARNOVA/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'api',
     'rest_framework.authtoken',
     'rest_framework',
 ]
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import api_translate
-
-urlpatterns = [
-    path('translate', api_translate, name='api_translate'),
-]
