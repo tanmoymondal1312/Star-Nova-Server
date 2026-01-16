79aaee714d67c751e2218d999a690f2c5edf56fd
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-01-16 21:55:25 +0600

    User data create and get Implemented

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import UserData
 
+from django.contrib import admin
+from .models import UserData
+
+
 @admin.register(UserData)
 class UserDataAdmin(admin.ModelAdmin):
-    list_display = ('user', 'mobile_number', 'is_paid')
-    search_fields = ('user__username', 'mobile_number')
-    list_filter = ('is_paid',)
+    list_display = (
+        'user',
+        'language',
+        'experience_level',
+        'is_paid'
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+LANGUAGE_CHOICES = [
+    ('en', 'English'),
+    ('hi', 'Hindi'),
+    ('bn', 'Bangla'),
+    ('ta', 'Tamil'),
+    ('te', 'Telugu'),
+]
+EXPERIENCE_LEVEL_CHOICES = [
+    ('beginner', 'Beginner'),
+    ('intermediate', 'Intermediate'),
+    ('advanced', 'Advanced'),
+]
+
 class UserData(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
     mobile_number = models.CharField(max_length=15, blank=True, null=True)
     is_paid = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.user.username
+    language = models.CharField(
+        max_length=2,
+        choices=LANGUAGE_CHOICES,
+    )
 
-   
+    name = models.CharField(max_length=255, blank=True, null=True)
+    experience_level = models.CharField(
+        max_length=20,
+        choices=EXPERIENCE_LEVEL_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    age = models.PositiveIntegerField(blank=True, null=True)
+    classification = models.CharField(max_length=100, blank=True, null=True)
+
+
+ 
